4c97788d0e1e475544dfa3a2824ddbd7bb413b23
fk <inflo@web.de>
2021-07-16 23:09:31 +0200

    broadcast all 21 points

--- ros2_galactic_mediapipe_hands/ros2_galactic_mediapipe_hands.py
+++ ros2_galactic_mediapipe_hands/ros2_galactic_mediapipe_hands.py
@@ -96,6 +96,72 @@ class MinimalPublisher(Node):
             odom_trans3 = TransformStamped()
             odom_trans3.header.frame_id = 'intermediate1'
             odom_trans3.child_frame_id = 'distal1'
+            
+            odom_trans4 = TransformStamped()
+            odom_trans4.header.frame_id = 'wrist1'
+            odom_trans4.child_frame_id = 'thumb_cmc'
+            
+            odom_trans5 = TransformStamped()
+            odom_trans5.header.frame_id = 'thumb_cmc'
+            odom_trans5.child_frame_id = 'thumb_mcp'
+            
+            odom_trans6 = TransformStamped()
+            odom_trans6.header.frame_id = 'thumb_mcp'
+            odom_trans6.child_frame_id = 'thumb_ip'
+            
+            odom_trans7 = TransformStamped()
+            odom_trans7.header.frame_id = 'thumb_ip'
+            odom_trans7.child_frame_id = 'thumb_tip'
+            
+            odom_trans8 = TransformStamped()
+            odom_trans8.header.frame_id = 'metacarpals1'
+            odom_trans8.child_frame_id = 'middle_finger_mcp'
+            
+            odom_trans9 = TransformStamped()
+            odom_trans9.header.frame_id = 'middle_finger_mcp'
+            odom_trans9.child_frame_id = 'middle_finger_pip'
+            
+            odom_trans10 = TransformStamped()
+            odom_trans10.header.frame_id = 'middle_finger_pip'
+            odom_trans10.child_frame_id = 'middle_finger_dip'
+            
+            odom_trans11 = TransformStamped()
+            odom_trans11.header.frame_id = 'middle_finger_dip'
+            odom_trans11.child_frame_id = 'middle_finger_tip'
+            
+            odom_trans12 = TransformStamped()
+            odom_trans12.header.frame_id = 'middle_finger_mcp'
+            odom_trans12.child_frame_id = 'ring_finger_mcp'
+            
+            odom_trans13 = TransformStamped()
+            odom_trans13.header.frame_id = 'ring_finger_mcp'
+            odom_trans13.child_frame_id = 'ring_finger_pip'
+            
+            odom_trans14 = TransformStamped()
+            odom_trans14.header.frame_id = 'ring_finger_pip'
+            odom_trans14.child_frame_id = 'ring_finger_dip'
+            
+            odom_trans15 = TransformStamped()
+            odom_trans15.header.frame_id = 'ring_finger_dip'
+            odom_trans15.child_frame_id = 'ring_finger_tip'
+            
+            odom_trans16 = TransformStamped()
+            odom_trans16.header.frame_id = 'ring_finger_mcp'
+            odom_trans16.child_frame_id = 'pinky_mcp'
+            
+            odom_trans17 = TransformStamped()
+            odom_trans17.header.frame_id = 'pinky_mcp'
+            odom_trans17.child_frame_id = 'pinky_pip'
+            
+            odom_trans18 = TransformStamped()
+            odom_trans18.header.frame_id = 'pinky_pip'
+            odom_trans18.child_frame_id = 'pinky_dip'
+            
+            odom_trans19 = TransformStamped()
+            odom_trans19.header.frame_id = 'pinky_dip'
+            odom_trans19.child_frame_id = 'pinky_tip'
+                
+                
                     
             msg = Hand()
             if results.multi_hand_landmarks:
@@ -156,19 +222,138 @@ class MinimalPublisher(Node):
                     odom_trans3.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y * iheight
                     odom_trans3.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].z
                     odom_trans3.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    #new
+                    odom_trans4.header.stamp = now.to_msg()
+                    odom_trans4.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_CMC].x * iwidth
+                    odom_trans4.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_CMC].y * iheight
+                    odom_trans4.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_CMC].z
+                    odom_trans4.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans5.header.stamp = now.to_msg()
+                    odom_trans5.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_MCP].x * iwidth
+                    odom_trans5.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_MCP].y * iheight
+                    odom_trans5.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_MCP].z
+                    odom_trans5.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans6.header.stamp = now.to_msg()
+                    odom_trans6.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_IP].x * iwidth
+                    odom_trans6.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_IP].y * iheight
+                    odom_trans6.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_IP].z
+                    odom_trans6.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans7.header.stamp = now.to_msg()
+                    odom_trans7.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_TIP].x * iwidth
+                    odom_trans7.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_TIP].y * iheight
+                    odom_trans7.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.THUMB_TIP].z
+                    odom_trans7.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans8.header.stamp = now.to_msg()
+                    odom_trans8.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x * iwidth
+                    odom_trans8.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y * iheight
+                    odom_trans8.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].z
+                    odom_trans8.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans9.header.stamp = now.to_msg()
+                    odom_trans9.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_PIP].x * iwidth
+                    odom_trans9.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_PIP].y * iheight
+                    odom_trans9.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_PIP].z
+                    odom_trans9.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans10.header.stamp = now.to_msg()
+                    odom_trans10.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_DIP].x * iwidth
+                    odom_trans10.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y * iheight
+                    odom_trans10.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_DIP].z
+                    odom_trans10.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans11.header.stamp = now.to_msg()
+                    odom_trans11.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * iwidth
+                    odom_trans11.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * iheight
+                    odom_trans11.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_TIP].z
+                    odom_trans11.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans12.header.stamp = now.to_msg()
+                    odom_trans12.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_MCP].x * iwidth
+                    odom_trans12.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_MCP].y * iheight
+                    odom_trans12.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_MCP].z
+                    odom_trans12.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans13.header.stamp = now.to_msg()
+                    odom_trans13.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_PIP].x * iwidth
+                    odom_trans13.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_PIP].y * iheight
+                    odom_trans13.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_PIP].z
+                    odom_trans13.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans14.header.stamp = now.to_msg()
+                    odom_trans14.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_DIP].x * iwidth
+                    odom_trans14.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_DIP].y * iheight
+                    odom_trans14.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_DIP].z
+                    odom_trans14.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                
+                    odom_trans15.header.stamp = now.to_msg()
+                    odom_trans15.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_TIP].x * iwidth
+                    odom_trans15.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_TIP].y * iheight
+                    odom_trans15.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.RING_FINGER_TIP].z
+                    odom_trans15.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans16.header.stamp = now.to_msg()
+                    odom_trans16.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_MCP].x * iwidth
+                    odom_trans16.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_MCP].y * iheight
+                    odom_trans16.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_MCP].z
+                    odom_trans16.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans17.header.stamp = now.to_msg()
+                    odom_trans17.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_PIP].x * iwidth
+                    odom_trans17.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_PIP].y * iheight
+                    odom_trans17.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_PIP].z
+                    odom_trans17.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans18.header.stamp = now.to_msg()
+                    odom_trans18.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_DIP].x * iwidth
+                    odom_trans18.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_DIP].y * iheight
+                    odom_trans18.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_DIP].z
+                    odom_trans18.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                    
+                    odom_trans19.header.stamp = now.to_msg()
+                    odom_trans19.transform.translation.x = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_TIP].x * iwidth
+                    odom_trans19.transform.translation.y = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_TIP].y * iheight
+                    odom_trans19.transform.translation.z = hand_landmarks.landmark[self.mp_hands.HandLandmark.PINKY_TIP].z
+                    odom_trans19.transform.rotation = self.euler_to_quaternion(0, 0, 0) # roll,pitch,yaw
+                
+                    
+                    
+                    
+                    
                     # odom_trans.transform.rotation = \
                         # euler_to_quaternion(0, 0, angle + pi/2) # roll,pitch,yaw
                     
 
-                    
-                    # self.mp_drawing.draw_landmarks(
-                    #     image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+                    #draw landmarsk for hand_image topic
+                    self.mp_drawing.draw_landmarks(
+                        image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
             
             self.broadcaster.sendTransform(odom_trans)
             self.broadcaster.sendTransform(odom_trans0)
             self.broadcaster.sendTransform(odom_trans1)
             self.broadcaster.sendTransform(odom_trans2)
             self.broadcaster.sendTransform(odom_trans3)
+            self.broadcaster.sendTransform(odom_trans4)
+            self.broadcaster.sendTransform(odom_trans5)
+            self.broadcaster.sendTransform(odom_trans6)
+            self.broadcaster.sendTransform(odom_trans7)
+            self.broadcaster.sendTransform(odom_trans8)
+            self.broadcaster.sendTransform(odom_trans9)
+            self.broadcaster.sendTransform(odom_trans10)
+            self.broadcaster.sendTransform(odom_trans11)
+            self.broadcaster.sendTransform(odom_trans12)
+            self.broadcaster.sendTransform(odom_trans13)
+            self.broadcaster.sendTransform(odom_trans14)
+            self.broadcaster.sendTransform(odom_trans15)
+            self.broadcaster.sendTransform(odom_trans16)
+            self.broadcaster.sendTransform(odom_trans17)
+            self.broadcaster.sendTransform(odom_trans18)
+            self.broadcaster.sendTransform(odom_trans19)
+            
 
             self.hand_image_publisher_.publish(bridge.cv2_to_imgmsg(image, "bgr8"))
             self.hand_msg_publisher_.publish(msg)
